c208aad5b698bb06e13c21977a0ee885a34ee0cb
ThavratNath <thavratnath@gmail.com>
2024-08-08 12:12:40 +0700

    Add files via upload

---
+++ Comparision_Operator.py
@@ -0,0 +1,33 @@
+
+'''Comparision operator''''''We use it when we want a group of code to be printed'''
+'''Example: 
+- >, <, >=, <=
+- == : when the two value is the same on the left and the right side is the same. 
+- = : is called the assignment operator. We give the value to the variable
+Ex: 2==2: true
+    x = 2: x'''
+    
+# print(f"10>3 is {10>3}.")
+# print(f"3<7 is {3<7}.")
+# print(f"5==5 is {5==5}.")
+# print(f"5>=4 is {5>4}.")
+# print(f"7<=9 is {7<=9}.")
+# print(f"3!=4 is {3!=4}.")
+
+# a, b, c = 30, 40, 50
+# print(a, b, c)
+
+# my_id, name, gender =int(input("Put your ID: ")), input("Name: "), input("Gender: ")
+# print(f"ID: {my_id + 1}")
+# print(f"Name: {name}")
+# print(f"Gender: {gender}")          # business logic
+
+# bank = 100
+# bank = bank + int(input("Enter your new salary: "))
+# print(f"My total salary is {bank} $.")
+
+
+m = 250 
+m -= int(input("How much did you spend this month?: "))
+print(f"So, the money in your bank account is {m} $.")
+
